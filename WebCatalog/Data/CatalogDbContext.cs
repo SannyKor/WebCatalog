3e82e1bdbf68f5a4dd6520f634db91c66a3b8d93
SannyKor <sanny0787@gmail.com>
2025-11-03 02:43:39 +0200

    feat(dbContext): add configurations

---
+++ WebCatalog/Data/CatalogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCatalog.Models;
+
+namespace WebCatalog.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext (DbContextOptions<CatalogDbContext> options) : base(options) { }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<QuantityHistory> QuantityHistories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UnitConfiguration());
+        }
+
+    }
+}
---
+++ WebCatalog/Data/Configurations/UnitConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebCatalog.Models;
+
+
+namespace WebCatalog.Data.Configurations
+{
+    public class UnitConfigurations : IEntityTypeConfiguration<Unit>
+    {
+        public void Configure(EntityTypeBuilder<Unit> entity)
+        {
+
+        }
+
+    }
+}
